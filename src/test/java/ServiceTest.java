6af8314c102cfd15f8fa39529772171b817ed8bb
Utilisateur <user@debian.arcahe.ovh>
2020-03-28 16:39:30 +0100

    test variables

---
+++ src/test/java/ServiceTest.java
@@ -0,0 +1,21 @@
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ServiceTest {
+    
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    
+    
+ 
+
+}
